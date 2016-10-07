e54ce0c8c2da0cdb06b30559c8fd1bef18dffec1
Rohit507 <rohit507@gmail.com>
2016-10-06 22:19:54 -0700

    Moves to LTS 7.2 (w/ ghc 8) and added initial implementation of ranges.

--- lib/EDG/Algebra/GenOrd.hs
+++ lib/EDG/Algebra/GenOrd.hs
@@ -4,6 +4,76 @@
 --   will grow as a design is added to.
 module EDG.Algebra.GenOrd where
 
+import Algebra.PartialOrd
 import Algebra.Lattice
 
-class (Eq a, BoundedLattice a) => GenOrd a
+-- | Adds testers to the lattice functions allowing us to collapse multiple
+--   possible representations of bottom and top.
+class (Eq a,PartialOrd a, BoundedLattice a) => LatticeTest a where
+  isBottom :: a -> Bool
+  isTop    :: a -> Bool
+
+-- | Class that specifies a generality order, in particular an order where
+--   `top` means "failure", `bottom` means "anything/no information/ignorance",
+--   `join` means "unify" , and `meet` means "generalize"
+class (LatticeTest a) => GenOrd a where
+
+any :: GenOrd a => a
+any = bottom
+
+fail :: GenOrd a => a
+fail = top
+
+unify :: GenOrd a => a -> a -> a
+unify = (\/)
+
+generalize :: GenOrd a => a -> a -> a
+generalize = (/\)
+
+-- | Wrapper for a datatype with equality that places a top and bottom on
+--   it such that no two elements of the original datatype have an ordering
+--   relative to each other.
+data ID a = Top | ID a | Bottom
+  deriving (Eq, Ord, Show, Read)
+
+instance Eq a => PartialOrd (ID a) where
+  leq _ Top = True
+  leq Top _ = False
+  leq Bottom _ = True
+  leq _ Bottom = False
+  leq (ID a) (ID b)
+    | a == b = True
+    | otherwise = False
+
+instance Eq a => JoinSemiLattice (ID a) where
+  (\/) Top _ = Top
+  (\/) _ Top = Top
+  (\/) Bottom b = b
+  (\/) a Bottom = a
+  (\/) (ID a) (ID b)
+    | a == b = ID a
+    | otherwise = Top
+
+instance Eq a => MeetSemiLattice (ID a) where
+  (/\) Top a = a
+  (/\) b Top = b
+  (/\) Bottom _ = Bottom
+  (/\) _ Bottom = Bottom
+  (/\) (ID a) (ID b)
+    | a == b = ID a
+    | otherwise = Bottom
+
+instance Eq a => BoundedJoinSemiLattice (ID a) where
+  bottom = Bottom
+
+instance Eq a => BoundedMeetSemiLattice (ID a) where
+  top = Top
+
+instance Eq a => Lattice (ID a)
+instance Eq a => BoundedLattice (ID a)
+
+instance Eq a => LatticeTest(ID a) where
+  isBottom = (== Bottom)
+  isTop = (== Top)
+
+instance Eq a => GenOrd (ID a)
---
+++ lib/EDG/Library/Types.hs
@@ -0,0 +1,13 @@
+
+-- | A bunch of basic types and their corresponding kinds as specified by
+--   their instances of GenOrd
+module EDG.Library.Types where
+
+
+
+-- TODO:
+--  - IDs
+--  - Integers
+--  - Reals
+--  - Sets
+--  - Records
