39f9cfdd8e33e509650b423329060173bca21190
Rohit507 <rohit507@gmail.com>
2017-02-01 19:34:50 -0800

    Added some TODOs and cleaned up Data.EqMap

--- lib/Data/EqMap.hs
+++ lib/Data/EqMap.hs
@@ -10,6 +10,8 @@ import qualified Data.Set as Set
 import Data.Bimap (Bimap)
 import qualified Data.Bimap as Bimap
 
+import Data.Maybe
+
 import Control.Newtype
 import Control.Newtype.Util
 
@@ -35,44 +37,29 @@ instance Num EqClass where
  signum = over EqClass signum
  fromInteger = EqClass . fromInteger
 
-data EqMap k v = EqMap {
-    toEq     :: Map k EqClass
-  , fromEq   :: Bimap EqClass (Set k)
-  , toVal    :: Bimap EqClass v
-  }
-
--- instance (Ord k,Ord v) => Functor (EqMap k) where
-
--- instance (Ord k) => Foldable (EqMap k) where
---   foldMap f em@EqMap{..} = foldMap f toVal
+-- | An EqMap is a map where you can specify that the keys for a pair of
+--   values are actually equal, at which point the map will use a join to
+--   combine the pair of values.
 --
--- instance (Ord k) => Traversable (EqMap k) where
---   traverse :: Applicative f => (a -> f b) -> EqMap k a -> f (EqMap k b)
---   traverse f em@EqMap{..} = (\ v -> em{toVal = v}) <$> traverse f toVal
-
--- instance (Ord k, Eq v) => Eq (EqMap k v) where
-
--- instance (Ord k) => IsList (EqMap k) where
-
--- instance Show (EqMap k v) where
-
--- instance Read (EqMap k v) where
-
--- instance Monoid (EqMap k v) where
-
--- instance Semigroup (EqMap k v) where
-
--- instance JoinSemiLattice (EqMap k v) where
-
--- instance BoundedJoinSemiLattice (EqMap k v) where
-
--- instance PartialOrd (EqMap k v) where
+--   This is great for something like type unification, you just walk through
+--   your system finding types, and when you see them connected you tell the
+--   map that they're equal.
+--
+--   This makes sure all the relevant equality classes are collected nicely
+--   together, and are completely transitive.
+--
+data EqMap k v = EqMap {
+    toEq  :: Map k EqClass
+  , toKey :: Bimap EqClass (Set k)
+  , toVal :: Bimap EqClass v
+  }
 
 -- * Operators
 
--- Extract Element from eqmap, calls error is value not in map
+-- Extract Element from eqmap, returns bottom if the value isn't in the
+-- map, as it is assumed all unconnected values are simply bottom.
 (!) :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> k -> v
-(!) m k = let Just v = lookup k m in v
+(!) m k = fromMaybe bottom (lookup k m)
 
 -- * Query
 
@@ -90,7 +77,7 @@ numKeys EqMap{..} = Map.size toEq
 
 -- | Number of values/equality classes
 numVals :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> Int
-numVals EqMap{..} = Bimap.size fromEq
+numVals EqMap{..} = Bimap.size toKey
 
 -- | Is Key in the EqMap?
 member :: (Ord k, Ord v, BoundedJoinSemiLattice v) => k -> EqMap k v -> Bool
@@ -106,15 +93,15 @@ lookup k EqMap{..} = do
   id <- Map.lookup k toEq
   Bimap.lookup id toVal
 
--- | Look up an element but return a default if it's not in the map
+-- | Look up an element but return a default if it's not in the map.
 findWithDefault :: (Ord k, Ord v, BoundedJoinSemiLattice v) => v -> k -> EqMap k v -> v
-findWithDefault = undefined
+findWithDefault d k em = fromMaybe d (lookup k em)
 
 -- * Construction
 
 -- | the empty eqmap
 empty :: (Ord k, Ord v, BoundedJoinSemiLattice v) =>  EqMap k v
-empty = EqMap {toEq = Map.empty, fromEq = Bimap.empty, toVal = Bimap.empty}
+empty = EqMap {toEq = Map.empty, toKey = Bimap.empty, toVal = Bimap.empty}
 
 -- | the singleton eqmap
 singleton :: (Ord k, Ord v, BoundedJoinSemiLattice v) => k -> v -> EqMap k v
@@ -127,12 +114,10 @@ singleton k v = insert k v empty
 --   existing value.
 insert :: (Ord k, Ord v, BoundedJoinSemiLattice v) => k -> v -> EqMap k v -> EqMap k v
 insert k v em@EqMap{..} = case Map.lookup k toEq of
-  Just id -> em{ toEq  = Map.insert k id toEq
-               , toVal = Bimap.adjust (\/ v) id toVal
-               }
-  Nothing -> EqMap { toEq   = Map.insert k newId toEq
-                   , fromEq = Bimap.insert newId (Set.singleton k) fromEq
-                   , toVal  = Bimap.insert newId v toVal
+  Just id -> em{ toVal = Bimap.adjust (\/ v) id toVal }
+  Nothing -> EqMap { toEq  = Map.insert k newId toEq
+                   , toKey = Bimap.insert newId (Set.singleton k) toKey
+                   , toVal = Bimap.insert newId v toVal
                    }
     where
       newId = getNextClass em
@@ -142,21 +127,23 @@ insert k v em@EqMap{..} = case Map.lookup k toEq of
 -- insert both elements as bottom
 -- getIDa & IDb
 --   toEq .= all elems of IDb set to IDa
---   fromEq .= delete IDb . (IDa += IDb)
+--   toKey .= delete IDb . (IDa += IDb)
 --   toVal .= delete IDb . (IDa = IDa \/ IDb)
+--
+--
 setEq :: (Ord k, Ord v, BoundedJoinSemiLattice v) => k -> k -> EqMap k v -> EqMap k v
 setEq k k' em
-  | k == k' = insert k bottom em
+  | k == k'   = insert k bottom em
   | otherwise = set . (insert k bottom) . (insert k' bottom) $ em
   where
     set em@EqMap{..} =
       let Just id   = Map.lookup k  toEq in
       let Just id'  = Map.lookup k' toEq in
-      let Just set' = Bimap.lookup id' fromEq in
+      let Just set' = Bimap.lookup id' toKey in
       let Just val' = Bimap.lookup id' toVal in
       EqMap { toEq   = Map.map (replace id' id) toEq
-            , fromEq = Bimap.delete id' . Bimap.adjust (Set.union set') id $ fromEq
-            , toVal  = Bimap.delete id' . Bimap.adjust (\/ val') id $ toVal
+            , toKey  = Bimap.delete id' . Bimap.adjust (Set.union set') id $ toKey
+            , toVal  = Bimap.delete id' . Bimap.adjust (\/ val')        id $ toVal
             }
 
     replace a b k
@@ -165,13 +152,18 @@ setEq k k' em
 
 -- * Combine
 
-union :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> EqMap k v -> EqMap k v
-union = undefined
-
-unions :: (Ord k, Ord v, BoundedJoinSemiLattice v) => [EqMap k v] -> EqMap k v
-unions = foldr union empty
+-- TODO :: I'm not entirely sure what a union of two sets would mean? inserting
+--         each key, equality relation, and value in one map into the other?
+-- union :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> EqMap k v -> EqMap k v
+-- union = undefined
+--
+-- unions :: (Ord k, Ord v, BoundedJoinSemiLattice v) => [EqMap k v] -> EqMap k v
+-- unions = foldr union empty
 
 -- * Traversal
+
+-- TODO :: Just write instances for Functor, Foldable, and Traversable the
+--         rest will be easy to derive from there.
 
 -- map :: (v -> v') -> EqMap k v -> EqMap k v'
 -- map = undefined
@@ -181,15 +173,28 @@ unions = foldr union empty
 
 -- * Conversion
 
+-- | Get all the values in the in the EqMap, this won't duplicate equality
+--   classes in the resulting list.
 elems :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> [v]
 elems EqMap{..} = Bimap.keysR toVal
 
+-- | Get a list of keys in the EqMap
 keys :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> [k]
 keys EqMap{..} = Map.keys toEq
 
+-- | Gets a list of equality classes and make a list out of them,
+--
+--   TODO :: This should be the base of the EqMap's IsList instance when you
+--           get around to writing that up.
+--
 classes :: (Ord k, Ord v, BoundedJoinSemiLattice v) => EqMap k v -> [(Set k,v)]
-classes EqMap{..} = map (\ id -> (fromEq Bimap.! id, toVal Bimap.! id)) $ Bimap.keys fromEq
+classes EqMap{..} = map (\ id -> (toKey Bimap.! id, toVal Bimap.! id)) $ Bimap.keys toKey
 
+-- | Generates an equality map from a set of equality classes, so that the
+--
+--   TODO :: This should also be part of the relevant IsList instance when you
+--           write that.
+--
 fromClasses :: (Ord k, Ord v, BoundedJoinSemiLattice v) => [(Set k,v)] -> EqMap k v
 fromClasses = foldr insertClass empty
   where
@@ -202,5 +207,7 @@ fromClasses = foldr insertClass empty
 
 -- Internal Functions
 
+-- | Given an EqMap, find an unused key. I make no guarentees about the space
+--   of keys being filled in, just that this gets you an as yet unused key.
 getNextClass :: (Ord k, Ord v, JoinSemiLattice v) =>  EqMap k v -> EqClass
-getNextClass EqMap{..} =  (+ 1) . fst $ Bimap.findMax fromEq
+getNextClass EqMap{..} =  (+ 1) . fst $ Bimap.findMax toKey
