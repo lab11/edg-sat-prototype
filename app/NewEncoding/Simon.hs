77341db336e45f4bdaf5c92b1ac208a639f27e4e
Richard Lin <richard.lin@berkeley.edu>
2017-03-31 17:44:15 -0700

    Top level UART fun

--- app/NewEncoding/Simon.hs
+++ app/NewEncoding/Simon.hs
@@ -11,16 +11,21 @@ import Control.Monad
 testLibrary :: EDGLibrary
 testLibrary = EDGLibrary{
   modules = [
-    ("button", 4, button),
-    ("led", 4, led),
+    ("i2cPower", 1, i2cPower),
+    ("button", 0, button),
+    ("led", 0, led),
+    ("tmp102", 1, tmp102),
+    ("lcd3v3", 1, serialLcd16x2_3v3),
+    ("lcd5v", 1, serialLcd16x2_5v),
     ("mcu", 1, mcu)
     ],
   links = [
-    ("apiLink", 8, apiLink),
-    ("powerLink", 2, powerLink 4),
+    ("apiLink", 5, apiLink),
+    ("powerLink", 2, powerLink 8),
     ("digitalBidirLink", 0, digitalBidirLink),
     ("digitalBidirSinkLink", 1, digitalBidirSinkLink),
     ("digitalBidirSourceLink", 1, digitalBidirSourceLink),
+    ("uartLink", 1, uartLink),
     ("i2cLink", 1, i2cLink 2),
     ("digitalLink", 0, digitalLink)
     ]
@@ -57,16 +62,25 @@ seed = do
       ]
     return ()
 
-  tsense <- addPort "tsense" $ do
+  tsenses <- forM @[] [1..1] $ \ id -> addPort ("tsense" ++ (show id)) $ do
     apiConsumer
     setType [
-      "controlName" <:= StringV "tsense",
+      "controlName" <:= StringV ("tsense" ++ (show id)),
       "apiType" <:= StringV "temperatureSensor",
       "apiData" <:= Record unknown
       ]
     return ()
 
-  let allPorts = (buttons ++ leds ++ [tsense])
+  lcd <- addPort "lcd" $ do
+    apiConsumer
+    setType [
+      "controlName" <:= StringV "lcd",
+      "apiType" <:= StringV "characterLcd",
+      "apiData" <:= Record unknown
+      ]
+    return ()
+
+  let allPorts = (buttons ++ leds ++ tsenses ++ [lcd])
   forM allPorts (\ portId -> constrain $ port portId connected)
   forM allPorts (\ portId -> constrain $ port portId (typeVal "controlUid") :== (typeVal "controlUid"))
 
