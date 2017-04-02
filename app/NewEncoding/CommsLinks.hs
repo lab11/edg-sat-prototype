f95cd195a5c20f29351a3eb664d0dcfe79cd91e0
Rohit507 <rohit507@gmail.com>
2017-04-01 23:21:18 -0700

    apruced up i2c links

--- app/NewEncoding/CommsLinks.hs
+++ app/NewEncoding/CommsLinks.hs
@@ -30,27 +30,10 @@ uartLink = do
 
   ensureConnected [master, slave]
 
-  -- constrain $ port master connected
-  -- constrain $ port slave connected
-
-  -- constrain $ rSubset (port master (typeVal "voltage")) (port slave (typeVal "limitVoltage"))
-  -- constrain $ rSubset (port slave (typeVal "voltage")) (port master (typeVal "limitVoltage"))
-
-  -- constrain $ rSubset (port master (typeVal "current")) (port slave (typeVal "limitCurrent"))
-  -- constrain $ rSubset (port slave (typeVal "current")) (port master (typeVal "limitCurrent"))
+  setFieldsEq False [master, slave] ["controlUid", "controlName"]
 
   constrain $ voltageLevelCheck master slave
   constrain $ voltageLevelCheck slave  master
-
-  -- constrain $ port master (typeVal "0VoltageLevel") :<= port slave (typeVal "limit0VoltageLevel")
-  -- constrain $ port master (typeVal "1VoltageLevel") :>= port slave (typeVal "limit1VoltageLevel")
-  -- constrain $ port slave (typeVal "0VoltageLevel") :<= port master (typeVal "limit0VoltageLevel")
-  -- constrain $ port slave (typeVal "1VoltageLevel") :>= port master (typeVal "limit1VoltageLevel")
-
-  setFieldsEq False [master, slave] ["controlUid", "controlName"]
-
-  -- constrain $ port master (typeVal "controlUid") :== port slave (typeVal "controlUid")
-  -- constrain $ port master (typeVal "controlName") :== port slave (typeVal "controlName")
 
   constrain $ rNotDisjoint (port master (typeVal "baud")) (port slave (typeVal "baud"))
 
@@ -85,33 +68,32 @@ i2cLink numSlaves = do
         ]
       return()
 
-  constrain $ port power connected
-  constrain $ port master connected
+  ensureConnected [power, master]
   constrain $ Any (map (\ slave -> port slave connected) slaves)
 
-  constrain $ port master (typeVal "voltage.min") :== Lit (FloatV 0)
-  constrain $ port master (typeVal "voltage.max") :== port power (typeVal "voltage.max")
-  constrain $ rSubset (port master (typeVal "voltage")) (port master (typeVal "limitVoltage"))
-
-  constrain $ rSubset (port master (typeVal "current")) (port master (typeVal "limitCurrent"))
-
+  constrain $ port master (typeVal "voltage.min")   :== Lit (FloatV 0)
+  constrain $ port master (typeVal "voltage.max")   :== port power (typeVal "voltage.max")
   constrain $ port master (typeVal "1VoltageLevel") :== port power (typeVal "voltage.min")
 
-  forM slaves $ \ slave -> do
-    constrain $ port slave (typeVal "voltage") :== port master (typeVal "voltage")
-    constrain $ rSubset (port slave (typeVal "voltage")) (port slave (typeVal "limitVoltage"))
+  setFieldsEq False (master : slaves) ["voltage", "controlUid"]
 
-    constrain $ rSubset (port slave (typeVal "current")) (port slave (typeVal "limitCurrent"))
+  forM slaves $ \ slave -> do
+    -- constrain $ port slave (typeVal "voltage") :== port master (typeVal "voltage")
+    -- constrain $ rSubset (port slave (typeVal "voltage")) (port slave (typeVal "limitVoltage"))
+    -- constrain $ rSubset (port slave (typeVal "current")) (port slave (typeVal "limitCurrent"))
 
     constrain $ port slave (typeVal "1VoltageLevel") :== port power (typeVal "voltage.min")
-    constrain $ port master (typeVal "0VoltageLevel") :<= port slave (typeVal "limit0VoltageLevel")
-    constrain $ port slave (typeVal "0VoltageLevel") :<= port master (typeVal "limit0VoltageLevel")
-    constrain $ port master (typeVal "1VoltageLevel") :>= port slave (typeVal "limit1VoltageLevel")
-    constrain $ port slave (typeVal "1VoltageLevel") :>= port master (typeVal "limit1VoltageLevel")
+    constrain $ voltageLevelCheck slave  master
+    constrain $ voltageLevelCheck master slave
+
+    -- constrain $ port master (typeVal "0VoltageLevel") :<= port slave (typeVal "limit0VoltageLevel")
+    -- constrain $ port slave (typeVal "0VoltageLevel") :<= port master (typeVal "limit0VoltageLevel")
+    -- constrain $ port master (typeVal "1VoltageLevel") :>= port slave (typeVal "limit1VoltageLevel")
+    -- constrain $ port slave (typeVal "1VoltageLevel") :>= port master (typeVal "limit1VoltageLevel")
 
     constrain $ rNotDisjoint (port master (typeVal "frequency")) (port slave (typeVal "frequency"))
 
-    constrain $ port master (typeVal "controlUid") :== port slave (typeVal "controlUid")
+    -- constrain $ port master (typeVal "controlUid") :== port slave (typeVal "controlUid")
 
   forM (allPairs slaves) $ \ (slave1,slave2) -> do
     constrain $ port slave1 (typeVal "id") :/= port slave2 (typeVal "id")
@@ -143,11 +125,15 @@ i2cPower = do  -- aka the two pull up resistors
       ]
     return ()
 
-  constrain $ port vin connected
-  constrain $ port vout connected
+  ensureConnected [vin, vout]
 
-  constrain $ port vin (typeVal "current") :== port vout (typeVal "current")
-  constrain $ port vin (typeVal "voltage") :== port vout (typeVal "voltage")
-  constrain $ port vin (typeVal "limitVoltage") :== port vout (typeVal "limitVoltage")
+  -- constrain $ port vin connected
+  -- constrain $ port vout connected
+
+  setFieldsEq False [vin, vout] ["current", "voltage", "limitVoltage"]
+
+  -- constrain $ port vin (typeVal "current") :== port vout (typeVal "current")
+  -- constrain $ port vin (typeVal "voltage") :== port vout (typeVal "voltage")
+  -- constrain $ port vin (typeVal "limitVoltage") :== port vout (typeVal "limitVoltage")
 
   return ()
