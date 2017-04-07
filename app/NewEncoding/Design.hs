b69d68bed22b3b6c4c77a52f276ecdfc12591fe0
ducky <richard.lin@berkeley.edu>
2017-04-06 19:07:34 -0700

    Update simon & core design libs

--- app/NewEncoding/Design.hs
+++ app/NewEncoding/Design.hs
@@ -10,6 +10,7 @@ import NewEncoding.CommsLinks
 
 import NewEncoding.CommonModules
 import NewEncoding.ChipModules
+import NewEncoding.RedundantModules
 
 import NewEncoding.SwAdapters
 
@@ -29,30 +30,39 @@ fullLibrary = EDGLibrary{
     ("tmp102", 1, tmp102),
     ("lcd3v3", 1, serialLcd16x2_3v3),
     ("lcd5v", 1, serialLcd16x2_5v),
+    ("domeButton", 4, domeButton),
+    ("openLog", 1, openLog),
     ("sdcard", 1, sdcard),
+    ("qre1113Analog", 2, qre1113Analog),
 
     ("pwmControlFan", 1, powerControlFan),
     ("powerControlFan", 1, pwmControlFan),
 
     -- Interfaces
     ("pcf8575", 1, pcf8575),
+    ("tb6612fng", 1, tb6612fng),
     ("digitalAmplifier", 1, digitalAmplifier),
+    ("litButton", 4, litButton),
 
     ("fat32", 1, fat32),
 
     -- Microcontrollers
-    ("apm3v3", 1, apm3v3)
+    ("apm3v3", 1, apm3v3),
+    ("trinket3v3", 1, arduinoTrinket3v3)
     ],
   links = [
-    ("apiLink", 8, apiLink),
+    ("apiLink", 12, apiLink),
 
     ("powerLink", 3, powerLink 6),
     ("usbLink", 1, usbLink),
 
     ("digitalLink", 2, digitalLink),
     ("digitalBidirLink", 0, digitalBidirLink),
-    ("digitalBidirSinkLink", 4, digitalBidirSinkLink),
-    ("digitalBidirSourceLink", 4, digitalBidirSourceLink),
+    ("digitalBidirSinkLink", 6, digitalBidirSinkLink),
+    ("digitalBidirSourceLink", 6, digitalBidirSourceLink),
+
+    ("motorLink", 2, motorLink),
+    ("analogLink", 2, analogLink),
 
     ("spiLink", 1, spiLink 2),
     ("uartLink", 1, uartLink),
